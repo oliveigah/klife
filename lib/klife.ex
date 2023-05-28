901836e1e2e7f257c8ed892b5798c05b7f97779b
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2023-05-28 20:20:54 -0300

    chore: add klife_protocol dependency and compose files

--- lib/klife.ex
+++ lib/klife.ex
@@ -1,18 +1,14 @@
 defmodule Klife do
-  @moduledoc """
-  Documentation for `Klife`.
-  """
+  alias KlifeProtocol.Socket
+  alias KlifeProtocol.Messages
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Klife.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def test do
+    {:ok, socket} = Socket.connect("localhost", 19092, backend: :gen_tcp, active: false)
+    version = 0
+    input = %{headers: %{correlation_id: 123}, content: %{}}
+    serialized_msg = Messages.ApiVersions.serialize_request(input, version)
+    :ok = :gen_tcp.send(socket, serialized_msg)
+    {:ok, received_data} = :gen_tcp.recv(socket, 0, 5_000)
+    Messages.ApiVersions.deserialize_response(received_data, version)
   end
 end
---
+++ stop-kafka.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose -f ./test/compose_files/docker-compose-kafka.yml down --volumes
